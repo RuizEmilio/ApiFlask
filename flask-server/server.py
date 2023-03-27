25b47a4e34a81602281f3cae024cb064dbfdf0d1
RuizEmilio <eruizb2000@alumno.ipn.mx>
2023-03-27 01:57:57 -0600

    FlaskAPI

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,14 +1,40 @@
 from flask import Flask
+import pydot
 
 app = Flask(__name__)
 
-@app.route('/members' , methods = ['POST'])
+# THE GRAPH FROM STRING
+dot_graph = pydot.graph_from_dot_file('grafo.dot')
+graph = dot_graph[0]
+first_subgraph = str(graph.get_subgraph_list()[0]) # FIRST SUBGRAPH
+
+# GET ROUTE OF MEMBERS
+@app.route('/members' , methods = ['GET' , 'POST'])
 def members() :
     return({
 
-        'members' : ['Member1','Member3','Member3']
+        'members' : ['Member1','Member2','Member3']
 
     })
 
+# GET ROUTE OF ANIMALS
+@app.route('/animals' , methods = ['GET' , 'POST'])
+def animals() :
+    return({
+
+        'animals' : ['dog' , 'cat' , 'turtle']
+
+    })
+
+# GET ROUTE OF GRAPH
+@app.route('/graphs' , methods = ['GET' , 'POST'])
+def graphs() :
+    return({
+
+        'graph' : [first_subgraph]
+
+    })
+
+# DEBUG THE APP
 if __name__ == '__main__' :
     app.run(debug = True)
